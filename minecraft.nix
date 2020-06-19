bb1a5009937c59fb08b35bb2c3274f8dbfe1d5d0
mcyster <mcyster@extole.com>
2020-06-19 16:21:19 -0700

    minecraft server setup

--- minecraft.nix
+++ minecraft.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  services.minecraft-server = {
+    enable = true;
+    eula = true;
+  };
+
   environment.systemPackages = with pkgs; [
     minecraft
     minecraft-server
